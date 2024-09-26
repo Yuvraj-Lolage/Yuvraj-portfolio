b64420546eb4045bc60f8ad7dcdc727638ebedf5
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-09-27 02:55:14 +0530

    added introduction home page

---
+++ src/components/textAnimation.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import "./TextAnimation.css"; // Make sure to import the CSS
+
+const TextAnimation = () => {
+  const words = ["UI/UX Designer"]; // Add more words as needed
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2000); // Delay in milliseconds (2 seconds)
+
+    return () => clearInterval(interval); // Clean up on component unmount
+  }, [words.length]);
+
+  return (
+    <div>
+      <span className="animated-text">{words[currentWordIndex]}</span>
+    </div>
+  );
+};
+
+export default TextAnimation;
--- src/pages/Homepage/portfolio_page.js
+++ src/pages/Homepage/portfolio_page.js
@@ -3,14 +3,30 @@ import { Link } from 'react-scroll';
 import '../Homepage/portfolio_page.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import TextAnimation from '../../components/textAnimation';
 const Homepage = () => {
     return (<>
-         
+
         <section id="home">
-            <h1 className='center-text'>Introduction</h1>
+            <h1 className='mt-5 name-text'>Hi, I am Yuvraj.</h1>
+            <div>
+                <TextAnimation />
+            </div>
+            <h1 className='based-text'>based in India.</h1>
+
+            <section className='desc-text'>
+                <h4>I love designing websites and user interfaces, doing freelance work and focusing on designing digital products that are easy on users and customers.</h4>
+            </section>
+
+            <section className='btn-section'>
+                <button className='btn btn-dark'>Get in touch</button>
+                <button className='btn btn-light'>Download Resume</button>
+            </section>
+
+
         </section>
         <section id="aboutme">
-            <h1 className='center-text'>About me</h1>
+            <h1 className='subtitle-text'>Who I am</h1>
         </section>
         <section id="services">
             <h1 className='center-text'>Services</h1>
@@ -26,5 +42,5 @@ const Homepage = () => {
         </section>
     </>);
 }
- 
+
 export default Homepage;
