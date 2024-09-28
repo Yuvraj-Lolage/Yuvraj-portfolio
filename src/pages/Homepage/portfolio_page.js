c6001afcf7abb262c25bc9521584efbfade5390d
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-09-28 19:21:23 +0530

    added scroll animation

--- src/pages/Homepage/portfolio_page.js
+++ src/pages/Homepage/portfolio_page.js
@@ -1,5 +1,58 @@
-import React from 'react'
-import { Link } from 'react-scroll';
+// import React from 'react'
+// import { Link } from 'react-scroll';
+// import '../Homepage/portfolio_page.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import TextAnimation from '../../components/TextAnimation/textAnimation';
+// import AboutUs from '../../components/AboutUs/aboutus';
+// import Services from '../../components/ProfessionalServices/services';
+// import { Projects } from '../../components/WorkShowcase/projects';
+// import { Contact } from '../../components/Contact/contact';
+// import { motion, useScroll } from 'framer-motion';
+// const Homepage = () => {
+
+//     const { scrollYProgress } = useScroll();
+
+//     return (<>
+
+//         <section id="home">
+//             <h1 className='mt-5 name-text'>Hi, I am Yuvraj.</h1>
+//             <div>
+//                 <TextAnimation />
+//             </div>
+//             <h1 className='based-text'>based in India.</h1>
+
+//             <section className='desc-text'>
+//                 <h4>I love designing websites and user interfaces, doing freelance work and focusing on designing digital products that are easy on users and customers.</h4>
+//             </section>
+
+//             <section className='btn-section'>
+//                 <button className='btn btn-dark'>Get in touch</button>
+//                 <button className='btn btn-light'>Download Resume</button>
+//             </section>
+
+
+//         </section>
+//         <section id="aboutme">
+//             <AboutUs/>
+//         </section>
+//         <section id="services">
+//             <Services/>
+//         </section>
+//         <section id="projects">
+//             <Projects/>
+//         </section>
+//         {/* <section id="experience">
+//             <h1 className='center-text'>experience</h1>
+//         </section> */}
+//         <section id="contact">
+//             <Contact/>
+//         </section>
+//     </>);
+// }
+
+// export default Homepage;
+import React from 'react';
 import '../Homepage/portfolio_page.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -8,43 +61,54 @@ import AboutUs from '../../components/AboutUs/aboutus';
 import Services from '../../components/ProfessionalServices/services';
 import { Projects } from '../../components/WorkShowcase/projects';
 import { Contact } from '../../components/Contact/contact';
+import { motion, useScroll } from 'framer-motion';
+
 const Homepage = () => {
-    return (<>
+    const { scrollYProgress } = useScroll();
 
-        <section id="home">
-            <h1 className='mt-5 name-text'>Hi, I am Yuvraj.</h1>
-            <div>
-                <TextAnimation />
-            </div>
-            <h1 className='based-text'>based in India.</h1>
+    return (
+        <>
+            {/* Scroll progress bar */}
+            <motion.div
+                className="progress-bar"
+                style={{ scaleX: scrollYProgress }}
+            />
+            <section id="home">
+                <h1 className='mt-5 name-text'>Hi, I am Yuvraj.</h1>
+                <div>
+                    <TextAnimation />
+                </div>
+                <h1 className='based-text'>based in India.</h1>
 
-            <section className='desc-text'>
-                <h4>I love designing websites and user interfaces, doing freelance work and focusing on designing digital products that are easy on users and customers.</h4>
+                <section className='desc-text'>
+                    <h4>I love designing websites and user interfaces, doing freelance work and focusing on designing digital products that are easy on users and customers.</h4>
+                </section>
+
+                <section className='btn-section'>
+                    <button className='btn btn-dark'>Get in touch</button>
+                    <button className='btn btn-light'>Download Resume</button>
+                </section>
+
+
             </section>
-
-            <section className='btn-section'>
-                <button className='btn btn-dark'>Get in touch</button>
-                <button className='btn btn-light'>Download Resume</button>
+            <section id="aboutme">
+                <AboutUs />
             </section>
-
-
-        </section>
-        <section id="aboutme">
-            <AboutUs/>
-        </section>
-        <section id="services">
-            <Services/>
-        </section>
-        <section id="projects">
-            <Projects/>
-        </section>
-        {/* <section id="experience">
+            <section id="services">
+                <Services />
+            </section>
+            <section id="projects">
+                <Projects />
+            </section>
+            {/* <section id="experience">
             <h1 className='center-text'>experience</h1>
         </section> */}
-        <section id="contact">
-            <Contact/>
-        </section>
-    </>);
-}
+            <section id="contact">
+                <Contact />
+            </section>
+
+        </>
+    );
+};
 
 export default Homepage;
--- src/pages/Navbar/navbar.js
+++ src/pages/Navbar/navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-scroll";
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../Navbar/navbar.css";
