1e6e50cb8ddb5a6b022adeaa872784a1c50b6f4f
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-10-02 01:01:21 +0530

    added hosted backend link

--- src/pages/AllProjects/allProjects.js
+++ src/pages/AllProjects/allProjects.js
@@ -3,23 +3,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../AllProjects/allProjects.css";
 import floatingButtonIcon from '../../assets/images/floating_button_icon.png';
-import arrowRight from '../../assets/images/arrow-right.png';
+import { Chip } from "@react-md/chip";
 import axios from 'axios';
 
 const AllProjects = () => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    const token = process.env.REACT_APP_JWT_AUTHORIZATION_TOKEN;
     useEffect(() => {
-        axios.get('http://localhost:4000')
+        axios.get('https://yuvraj-lolage-portfolio-backend-f28wh7kb5.vercel.app/',{
+            headers: {
+                Authorization: `Bearer ${token}` // Send the token in the Authorization header
+              }
+        })
             .then(response => {
                 setData(response.data);
                 console.log(response.data);
-                
+
                 setLoading(false);
             })
             .catch(err => {
                 setError(err.message);
+                console.log(err);
+                
                 setLoading(false);
             });
     }, []);
@@ -30,21 +38,32 @@ const AllProjects = () => {
     return (
         <>
             <section className="allprojects-section">
-        <span id="sub-title-text">Work Showcase</span>
-        <h1 id="title-text">Featured Designs & Developments</h1>
+                <Chip
+                    key=""
+                    selected=""
+                    className=""
+                    onClick={() => {}
+                        
+                    }
+                >
+                    MObile Application Development
+                </Chip>
+
+                <span id="sub-title-text">Work Showcase</span>
+                <h1 id="title-text">Featured Designs & Developments</h1>
                 <section className='project-tiles'>
                     {data.map((item, index) => (
                         <section className='project-row'>
-                        <div className='project'>
-                            <div class="card m-1">
-                                <button className="floating-button"><img className='floating-icon' src={floatingButtonIcon} /></button>
-                                <div class="card-body">
-                                    <img className='project-img' src={ item.projectThumbnail } />
+                            <div className='project'>
+                                <div class="card m-1">
+                                    <button className="floating-button"><img className='floating-icon' src={floatingButtonIcon} /></button>
+                                    <div class="card-body">
+                                        <img className='project-img' src={item.projectThumbnail} />
+                                    </div>
                                 </div>
+                                <span id='project-title'>{item.projectName}</span>
                             </div>
-                            <span id='project-title'>{ item.projectName }</span>
-                        </div>
-                    </section>
+                        </section>
                     ))}
                 </section>
             </section>
